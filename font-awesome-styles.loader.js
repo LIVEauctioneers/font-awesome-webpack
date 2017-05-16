3f9e9059655dd84640d39473a5d38164b6419eb6
Blake Rollins <brollins90@gmail.com>
2017-05-16 11:27:49 -0600

    la font awesome

--- font-awesome-styles.loader.js
+++ font-awesome-styles.loader.js
@@ -1,29 +1,13 @@
-var styles = [
-    'mixins',
-
-    'bordered-pulled',
-    'core',
-    'fixed-width',
-    'icons',
-    'larger',
-    'list',
-    'path',
-    'rotated-flipped',
-    'animated',
-    'stacked'
-];
 
 module.exports = function(content) {
     this.cacheable(true);
     var config = this.exec(content, this.resourcePath);
-    var start =
-            "@import          \"~font-awesome/less/variables.less\";\n"
-            + "@fa-font-path: \"~font-awesome/fonts/\";\n"
-            + "@import          \"./font-awesome.config.less\";\n";
-    source = start + styles.filter(function(style) {
-        return config.styles[style];
-    }).map(function(style) {
-        return "@import \"~font-awesome/less/" + style + ".less\";";
-    }).join("\n");
+    var source =
+        "@import \"~@liveauctioneers/font-awesome/less/variables.less\";\n"
+        + "@fa-font-path: \"~@liveauctioneers/font-awesome/fonts/\";\n"
+        + "@import \"./font-awesome.config.less\";\n"
+        + "@import \"~@liveauctioneers/font-awesome/less/core.less\";\n"
+        + "@import \"~@liveauctioneers/font-awesome/less/icons.less\";\n"
+        + "@import \"~@liveauctioneers/font-awesome/less/path.less\";\n";
     return source;
 };
